f679d1549a6ff356f7fadd16191ced1478f34819
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2016-04-10 22:37:55 -0400

    remove temporary methods

--- src/ast.hpp
+++ src/ast.hpp
@@ -19,45 +19,36 @@ private:
 public:
 	void set_data(long sline, long schar, long eline, long echar);
 	virtual void accept(std::shared_ptr<BFCC_Visitor>) = 0;
-	virtual char get_type() = 0;
 };
 
 class BFCC_Node_RAWAdd      : public BFCC_Node {
-	char get_type() { return '+';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWSub      : public BFCC_Node {
-	char get_type() { return '-';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWLeft     : public BFCC_Node {
-	char get_type() { return '<';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWRight    : public BFCC_Node {
-	char get_type() { return '>';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWDot      : public BFCC_Node {
-	char get_type() { return '.';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWComma    : public BFCC_Node {
-	char get_type() { return ',';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWLeftbrk  : public BFCC_Node {
-	char get_type() { return '[';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
 
 class BFCC_Node_RAWRightbrk : public BFCC_Node {
-	char get_type() { return ']';  };
 	void accept(std::shared_ptr<BFCC_Visitor>);
 };
