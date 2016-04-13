c675cba049733cedd3dbee303a59fac09b0b96a5
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2016-04-13 17:44:34 -0400

    remove the indentation

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -1,7 +1,6 @@
 #include "datastruct.hpp"
 #include <iostream>
 #include <cstring>
-#include <cctype>
 
 BFCC_Parameters argparser(int argc, char * argv[]) {
 	BFCC_Parameters p;
@@ -47,119 +46,110 @@ BFCC_Parameters argparser(int argc, char * argv[]) {
 
 	};
 
-	if (argc == 1) {
-		std::cout << "\033[1;31mError:\033[0m No options.\n";
-		display_help();
-		p.cont = false;
-	}
+	//Iterate through all arguments
+	//p.cont stores if the program should continue after parsing arguments
+	//or if it should quit due to an argument error.
+	for (int i = 1; i < argc; i++) {
 
-	else {
-		//Iterate through all arguments
-		//p.cont stores if the program should continue after parsing arguments
-		//or if it should quit due to an argument error.
-		for (int i = 1; i < argc; i++) {
-
-			//Flags with arguments
-			if (i+1 < argc) {
-				//Input files
-				if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input-file") == 0) {
-					p.ifilesrc = argv[++i];
-					continue;
-				}
-
-				//Output file
-				else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output-file") == 0) {
-					p.ofilesrc = argv[++i];
-					continue;
-				}
-
-				//Error file
-				else if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--error-file") == 0) {
-					p.errfilesrc = argv[++i];
-					continue;
-				}
-
-				//Input code
-				else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--code") == 0) {
-					p.argsrc = argv[++i];
-					continue;
-				}
-
-				//Verbosity
-				else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
-					i++;
-					if (argv[i][0] == '0') p.verbosity = 0;
-					else if (argv[i][0] == '1') p.verbosity = 1;
-					else if (argv[i][0] == '2') p.verbosity = 2;
-					else if (argv[i][0] == '3') p.verbosity = 3;
-					else {
-						std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid argument to " << argv[i-1] << ".\n";
-						p.cont = false;
-					}
-					continue;
-				}
-
-				else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--language") == 0) {
-					i++;
-					if (strcmp(argv[i], "brainfuck") == 0) p.ilang = I_BF_BRAINFUCK;
-					else if (strcmp(argv[i], "tinybf") == 0) p.ilang = I_BF_BRAINFUCK;
-					else {
-						std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid input language. Use --list-languages for more info.\n";
-						p.cont = false;
-					}
-					continue;
-				}
-
-				else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--target") == 0) {
-					i++;
-					if (strcmp(argv[i], "c") == 0) p.olang = O_FULL_C;
-					else if (strcmp(argv[i], "go") == 0) p.olang = O_FULL_GO;
-					else if (strcmp(argv[i], "java") == 0) p.olang = O_FULL_JAVA;
-					else if (strcmp(argv[i], "python") == 0) p.olang = O_FULL_PYTHON;
-
-					else if (strcmp(argv[i], "brainfuck") == 0) p.olang = O_BF_BRAINFUCK;
-					else {
-						std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid target language. Use --list-languages for more info.\n";
-						p.cont = false;
-					}
-					continue;
-				}
+		//Flags with arguments
+		if (i+1 < argc) {
+			//Input files
+			if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input-file") == 0) {
+				p.ifilesrc = argv[++i];
+				continue;
 			}
 
-			//Deal with arguments without an argument
-			if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0) {
-				p.debug = true;
+			//Output file
+			else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output-file") == 0) {
+				p.ofilesrc = argv[++i];
+				continue;
 			}
 
-			else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) { 
-				display_help();
-				p.cont = false;
+			//Error file
+			else if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--error-file") == 0) {
+				p.errfilesrc = argv[++i];
+				continue;
 			}
 
-			else if (strcmp(argv[i], "--list-languages") == 0) {
-				list_languages();
-				p.cont = false;
+			//Input code
+			else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--code") == 0) {
+				p.argsrc = argv[++i];
+				continue;
 			}
 
-			//If the argument doen't fit, throw error
-			else {
-				//These arguments need another argument
-				if ((strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input-file") == 0) ||
-					(strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output-file") == 0) ||
-					(strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--error-file") == 0) ||
-					(strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--code") == 0) ||
-					(strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--language") == 0) ||
-					(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--target") == 0) ) {
-					std::cout << "\033[1;31mError:\033[0m " << argv[i] << " requires an argument.\n";
-				}
-
-				//Generic error
-
+			//Verbosity
+			else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+				i++;
+				if (argv[i][0] == '0') p.verbosity = 0;
+				else if (argv[i][0] == '1') p.verbosity = 1;
+				else if (argv[i][0] == '2') p.verbosity = 2;
+				else if (argv[i][0] == '3') p.verbosity = 3;
 				else {
-					std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid option.\n";
+					std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid argument to " << argv[i-1] << ".\n";
+					p.cont = false;
 				}
-				p.cont = false;
+				continue;
 			}
+
+			else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--language") == 0) {
+				i++;
+				if (strcmp(argv[i], "brainfuck") == 0) p.ilang = I_BF_BRAINFUCK;
+				else if (strcmp(argv[i], "tinybf") == 0) p.ilang = I_BF_BRAINFUCK;
+				else {
+					std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid input language. Use --list-languages for more info.\n";
+					p.cont = false;
+				}
+				continue;
+			}
+
+			else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--target") == 0) {
+				i++;
+				if (strcmp(argv[i], "c") == 0) p.olang = O_FULL_C;
+				else if (strcmp(argv[i], "go") == 0) p.olang = O_FULL_GO;
+				else if (strcmp(argv[i], "java") == 0) p.olang = O_FULL_JAVA;
+				else if (strcmp(argv[i], "python") == 0) p.olang = O_FULL_PYTHON;
+
+				else if (strcmp(argv[i], "brainfuck") == 0) p.olang = O_BF_BRAINFUCK;
+				else {
+					std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid target language. Use --list-languages for more info.\n";
+					p.cont = false;
+				}
+				continue;
+			}
+		}
+
+		//Deal with arguments without an argument
+		if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0) {
+			p.debug = true;
+		}
+
+		else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) { 
+			display_help();
+			p.cont = false;
+		}
+
+		else if (strcmp(argv[i], "--list-languages") == 0) {
+			list_languages();
+			p.cont = false;
+		}
+
+		//If the argument doen't fit, throw error
+		else {
+			//These arguments need another argument
+			if ((strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input-file") == 0) ||
+				(strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output-file") == 0) ||
+				(strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--error-file") == 0) ||
+				(strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--code") == 0) ||
+				(strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--language") == 0) ||
+				(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--target") == 0) ) {
+				std::cout << "\033[1;31mError:\033[0m " << argv[i] << " requires an argument.\n";
+			}
+
+			//Generic error
+			else {
+				std::cout << "\033[1;31mError:\033[0m " << argv[i] << " is not a valid option.\n";
+			}
+			p.cont = false;
 		}
 	}
 	return p;
--- src/bfcc.cpp
+++ src/bfcc.cpp
@@ -15,19 +15,34 @@ int main (int argc, char * argv[]) {
 		return 1;
 	}
 
-	/*std::string input_data;
-	std::ifstream ifile (i);
-	if (ifile) {  
-		std::stringstream ss;
-		ss << ifile.rdbuf();
-		input_data = ss.str();
-		ifile.close();
-	}
-	else {
-		std::cerr << "\033[1;31mError:\033[0m file \"" << argv[1] << "\" can't be opened\n";
-		return EX_NOINPUT;
+	std::string input_data;
+	if (parameters.ifilesrc != NULL) {
+		std::ifstream ifile (parameters.ifilesrc); 
+		if (ifile) {  
+			std::stringstream ss;
+			ss << ifile.rdbuf();
+			input_data = ss.str();
+		}
+		else {
+			std::cerr << "\033[1;31mError:\033[0m file \"" << argv[1] << "\" can't be opened\n";
+			return 2;
+		}
 	}
 
+	else if (parameters.argsrc != NULL) {
+		input_data = parameters.argsrc;
+	}
+
+	else {
+		while (!std::cin.eof()) {
+			char c = std::cin.get();
+			input_data += c;
+		}
+	}
+
+	std::cout << input_data << std::endl;
+
+	/*
 	std::shared_ptr<BFCC_Lexer> lexer = std::make_shared<BFCC_Lexer_Brainfuck>();
 	auto nodelist = lexer->gen_node(input_data);
 
